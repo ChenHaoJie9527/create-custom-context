c1fdcc936420b3c1cf0c7ea2e85bb4cf993ac3f6
momotom <a18520145493@163.com>
2025-07-15 21:39:41 +0800

    test: createCustomContext > should work with useState hook

--- src/test/create-custom-context.spec.tsx
+++ src/test/create-custom-context.spec.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { useState } from 'react';
 import { describe, expect, it } from 'vitest';
 import { createCustomContext } from '../create-custom-context';
 
@@ -18,5 +19,33 @@ describe('createCustomContext', () => {
     );
 
     expect(screen.getByTestId('value')).toHaveTextContent('Hello, world!');
+  });
+
+  it('should work with useState hook', () => {
+    const [Provider, useContext] = createCustomContext(() => {
+      const [count, setCount] = useState(0);
+      return { count, setCount };
+    });
+
+    function Counter() {
+      const { count, setCount } = useContext();
+      return (
+        <div>
+          <span data-testid="count">{count}</span>
+          <button onClick={() => setCount(count + 1)} type="button">
+            Increment
+          </button>
+        </div>
+      );
+    }
+    render(
+      <Provider>
+        <Counter />
+      </Provider>
+    );
+
+    expect(screen.getByTestId('count')).toHaveTextContent('0');
+    fireEvent.click(screen.getByText('Increment'));
+    expect(screen.getByTestId('count')).toHaveTextContent('1');
   });
 });
